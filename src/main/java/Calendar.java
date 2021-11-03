8666535a3637f488a60872944df20f1a1861078b
yeny <syeeuns@gmail.com>
2021-11-03 10:21:22 +0900

    일정 등록, 검색 기능 구현

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -1,8 +1,19 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Calendar {
 
-  public static int monthDay(int year, int month) {
+  private Map<Date, String> planMap;
+
+  public Calendar() {
+    planMap = new HashMap<Date, String>();
+  }
+
+  public int monthDay(int year, int month) {
     if (List.of(1, 3, 5, 7, 8, 10, 12).contains(month)) {
       return 31;
     } else if (List.of(4, 6, 9, 11).contains(month)) {
@@ -12,7 +23,7 @@ public class Calendar {
     }
   }
 
-  public static boolean isLeap(int year) {
+  public boolean isLeap(int year) {
     return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
   }
 
@@ -21,7 +32,7 @@ public class Calendar {
   // 일월화수목금토 0123456 index
   // 윤년이면 지난해 1월1일 + 2
   // 평년이면 +1
-  public static int getYearFirstDay(int year) {
+  public int getYearFirstDay(int year) {
     int sum = 0;
     for (int i = 1583; i < year; i++) {
       if (isLeap(i)) {
@@ -35,7 +46,7 @@ public class Calendar {
 
   // 지난달 시작이 일요일이고, 30일이라면? 1, 8, 15, 22, 29, -> 다음달 시작은 화요일
   // 일수 % 7 만큼 지난 시작요일에 더해주면 됨
-  public static int getMonthFirstDay(int year, int month) {
+  public int getMonthFirstDay(int year, int month) {
     int sum = getYearFirstDay(year);
     for (int i = 1; i < month; i++) {
       sum += monthDay(year, i) % 7;
@@ -43,7 +54,7 @@ public class Calendar {
     return sum % 7;
   }
 
-  public static void printCalendar(int firstDay, int maxDay) {
+  public void printCalendar(int firstDay, int maxDay) {
     int cnt = 0;
     System.out.println("Sun Mon Tue Wed Thu Fri Sat");
     System.out.println("---------------------------");
@@ -59,5 +70,16 @@ public class Calendar {
       }
     }
     System.out.println();
+  }
+
+  public void registerPlan(String inputDate, String plan) throws ParseException {
+    Date date = new SimpleDateFormat("yyyy-MM-dd").parse(inputDate);
+    planMap.put(date, plan);
+  }
+
+  public String searchPlan(String inputDate) throws ParseException {
+    Date date = new SimpleDateFormat("yyyy-MM-dd").parse(inputDate);
+    String plan = planMap.get(date);
+    return plan;
   }
 }
--- src/main/java/Prompt.java
+++ src/main/java/Prompt.java
@@ -1,27 +1,30 @@
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
 
   private static final String PROMPT = "cal > ";
 
-  public static void main(String[] args) {
-    runPromprt();
+  public static void main(String[] args) throws ParseException {
+    Prompt prompt = new Prompt();
+    prompt.runPrompt();
   }
 
-  public static void runPromprt() {
+  public void runPrompt() throws ParseException {
     printMenu();
     Scanner sc = new Scanner(System.in);
+    Calendar cal = new Calendar();
 
     while (true) {
       System.out.println("명령 (1, 2, 3, h, q)");
       System.out.print(" > ");
       String cmd = sc.next();
       if (cmd.equals("1")) {
-        registerPlan(sc);
+        cmdRegister(sc, cal);
       } else if (cmd.equals("2")) {
-        searchPlan(sc);
+        cmdSearch(sc, cal);
       } else if (cmd.equals("3")) {
-        printCalendar(sc);
+        printCalendar(sc, cal);
       } else if (cmd.equals("h")) {
         printMenu();
       } else if (cmd.equals("q")) {
@@ -41,24 +44,25 @@ public class Prompt {
     System.out.println("+------------------------+");
   }
 
-  private static void registerPlan (Scanner sc) {
-    System.out.print("[일정 등록] 날짜를 입력하세요.\n > ");
-    String date = sc.nextLine();
-    // 날짜 찾기 로직 추가
+  private static void cmdRegister (Scanner sc, Calendar cal) throws ParseException {
+    System.out.println("[새 일정 등록]");
+    System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+    String date = sc.next();
+    sc.nextLine();
     System.out.print("일정을 입력하세요.\n > ");
-    String schedule = sc.nextLine();
-    // 일정 등록 로직 추가
+    String plan = sc.nextLine();
+    cal.registerPlan(date, plan);
+
     System.out.println("일정이 등록되었습니다.");
   }
 
-  private static void searchPlan (Scanner sc) {
-    System.out.print("[일정 검색] 날짜를 입력하세요.\n > ");
-    String date = sc.nextLine();
-    // 날짜 찾기 로직 추가
-    // 일정 검색 로직 추가
+  private static void cmdSearch(Scanner sc, Calendar cal) throws ParseException {
+    System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+    String date = sc.next();
+    System.out.println(cal.searchPlan(date));
   }
 
-  private static void printCalendar(Scanner sc) {
+  private static void printCalendar(Scanner sc, Calendar cal) {
     System.out.println("년, 월을 입력하세요. (띄어쓰기 구분)");
     System.out.print("cal > ");
     int year = Integer.parseInt(sc.next());
@@ -68,8 +72,8 @@ public class Prompt {
       System.exit(0);
     }
 
-    int maxDay = Calendar.monthDay(year, month);
+    int maxDay = cal.monthDay(year, month);
     System.out.printf("%d년 %d월은 %d일까지 있습니다.\n", year, month, maxDay);
-    Calendar.printCalendar(Calendar.getMonthFirstDay(year, month), maxDay);
+    cal.printCalendar(cal.getMonthFirstDay(year, month), maxDay);
   }
 }
