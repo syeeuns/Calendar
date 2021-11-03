7d211e6504f08e0d1bc0386a055f2a0e9aac7c88
yeny <syeeuns@gmail.com>
2021-11-03 09:52:53 +0900

    메뉴 생성, 달력 보기, 도움말, 종료 구현

--- src/main/java/Prompt.java
+++ src/main/java/Prompt.java
@@ -9,25 +9,67 @@ public class Prompt {
   }
 
   public static void runPromprt() {
+    printMenu();
     Scanner sc = new Scanner(System.in);
-    System.out.println("반복횟수를 입력하세요.");
-    System.out.print("cal > ");
-    int times = sc.nextInt();
-    for (int i = 0; i < times; i++) {
-      System.out.println("년, 월을 입력하세요. (띄어쓰기 구분)");
-      System.out.print("cal > ");
-      int year = Integer.parseInt(sc.next());
-      int month = Integer.parseInt(sc.next());
-      if (month == -1) {
+
+    while (true) {
+      System.out.println("명령 (1, 2, 3, h, q)");
+      System.out.print(" > ");
+      String cmd = sc.next();
+      if (cmd.equals("1")) {
+        registerPlan(sc);
+      } else if (cmd.equals("2")) {
+        searchPlan(sc);
+      } else if (cmd.equals("3")) {
+        printCalendar(sc);
+      } else if (cmd.equals("h")) {
+        printMenu();
+      } else if (cmd.equals("q")) {
         break;
       }
-      if (month > 12) {
-        continue;
-      }
-      int maxDay = Calendar.monthDay(year, month);
-      System.out.printf("%d년 %d월은 %d일까지 있습니다.\n", year, month, maxDay);
-      Calendar.printCalendar(Calendar.getMonthFirstDay(year, month), maxDay);
     }
+    System.out.println("Bye~");
     sc.close();
+  }
+
+  public static void printMenu() {
+    System.out.println("+------------------------+");
+    System.out.println("| 1. 일정 등록");
+    System.out.println("| 2. 일정 검색");
+    System.out.println("| 3. 달력 보기");
+    System.out.println("| h. 도움말 q. 종료");
+    System.out.println("+------------------------+");
+  }
+
+  private static void registerPlan (Scanner sc) {
+    System.out.print("[일정 등록] 날짜를 입력하세요.\n > ");
+    String date = sc.nextLine();
+    // 날짜 찾기 로직 추가
+    System.out.print("일정을 입력하세요.\n > ");
+    String schedule = sc.nextLine();
+    // 일정 등록 로직 추가
+    System.out.println("일정이 등록되었습니다.");
+  }
+
+  private static void searchPlan (Scanner sc) {
+    System.out.print("[일정 검색] 날짜를 입력하세요.\n > ");
+    String date = sc.nextLine();
+    // 날짜 찾기 로직 추가
+    // 일정 검색 로직 추가
+  }
+
+  private static void printCalendar(Scanner sc) {
+    System.out.println("년, 월을 입력하세요. (띄어쓰기 구분)");
+    System.out.print("cal > ");
+    int year = Integer.parseInt(sc.next());
+    int month = Integer.parseInt(sc.next());
+
+    if (month < 1 || month > 12) {
+      System.exit(0);
+    }
+
+    int maxDay = Calendar.monthDay(year, month);
+    System.out.printf("%d년 %d월은 %d일까지 있습니다.\n", year, month, maxDay);
+    Calendar.printCalendar(Calendar.getMonthFirstDay(year, month), maxDay);
   }
 }
